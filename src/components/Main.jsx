276742ab2a70eff87cd99df27fc4062c13bcfb25
sumansingh21 <sumans.bca1821@gmail.com>
2023-02-27 23:34:03 +0530

    Banner page added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Menu from './Homepage/Menu';
 import Navbar from './Homepage/Navbar';
+import Banner from './Homepage/Banner';
 
 const Main= () =>{
     return(
         <>
            <Navbar/>
            <Menu/>
+           <Banner/>
         </>
     )
 }
