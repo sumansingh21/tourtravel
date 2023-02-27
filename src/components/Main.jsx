83bc93e898ee476b1896e81468bf5438dc7cc023
sumansingh21 <sumans.bca1821@gmail.com>
2023-02-27 23:01:35 +0530

    Nav & Menu complete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,1 +1,14 @@
-check react
+import React from 'react';
+import Menu from './Homepage/Menu';
+import Navbar from './Homepage/Navbar';
+
+const Main= () =>{
+    return(
+        <>
+           <Navbar/>
+           <Menu/>
+        </>
+    )
+}
+
+export default Main;
